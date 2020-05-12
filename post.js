9138694ab0901ad550026fd7901373f2d60b5615
Starian Chen (Shang Hai Wei Chuang Ruan Jian) <v-stache@microsoft.com>
2020-05-12 20:39:16 +0800

    action/core

--- post.js
+++ post.js
@@ -1,1 +1,6 @@
 console.log('=============This is post log===================');
+const core = require('@actions/core');
+
+var pid = core.getState("pidToKill");
+
+console.log(`Action state result:${pid}`);
