71220c73e10e10ff38929e1b20d469e83954176f
Starian Chen (Shang Hai Wei Chuang Ruan Jian) <v-stache@microsoft.com>
2020-05-12 21:52:49 +0800

    add action/io

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const exec = require('@actions/exec');
 
 const glob = require('@actions/glob');
 
+const io = require('@actions/io');
+
 try{
     const nameToGreet=core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}`);
@@ -42,6 +44,8 @@ try{
 
     console.log("======================actions/glob=====================");
     asyncGlob();
+
+    console.log("====================@actions/io=======================");
 }
 catch(error)
 {
@@ -52,9 +56,13 @@ async function asyncExec() {
     await exec.exec('node', ['test.js', 'foo=bar']);
 }
 async function asyncGlob(){
-
+    // https://github.com/v-stache/hello-world-javascript-action/runs/667135669?check_suite_focus=true
     const patterns = ['**/*.js', '**/*.json'];
     const globber2 = await glob.create(patterns.join('\n'));
     const files = await globber2.glob();
     console.log(files);
+}
+async function copyFile(){
+    await io.mkdirP('folder1');
+    await io.mv('README.md', 'folder1/README.md');
 }
